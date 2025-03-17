d80ede73f64943fcaaaabfd9474956dd6b53b583
eustachekamala <eustachejared25@gmail.com>
2025-03-18 00:08:44 +0300

    updated the getCustomerSearch method to get user with two params for instance when we search John doe and retrive someting from the database

---
+++ app/(rs)/customers/CustomerTable.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import type { selectCustomerSchemaType } from "@/zod-schemas/customer";
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  data: selectCustomerSchemaType[];
+};
+
+export default function CustomerTable({ data }: Props) {
+  const router = useRouter();
+
+  const columnHeadersArray: Array<keyof selectCustomerSchemaType> = [
+    "firstName",
+    "lastName",
+    "email",
+    "phone",
+    "city",
+    "zip",
+  ];
+
+  const columnHelper = createColumnHelper<selectCustomerSchemaType>();
+
+  const columns = columnHeadersArray.map((columName) => {
+    return columnHelper.accessor(columName, {
+      id: columName,
+      header: columName[0].toUpperCase() + columName.slice(1),
+    });
+  });
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="mt-6 rounded-lg overflow-hidden border border-border">
+      <Table className="border">
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id} className="bg-gray-700/60">
+                  <div>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </div>
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              className="cursor-pointer hover:bg-gray-800/60 dark:hover:bg-gray-700/60"
+              onClick={() =>
+                router.push(`/customers/form?customerId=${row.original.id}`)
+              }
+            >
+                {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="border">
+                        { flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                        )}
+                    </TableCell>
+                ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- app/(rs)/customers/page.tsx
+++ app/(rs)/customers/page.tsx
@@ -1,21 +1,33 @@
-import CustomerSearch from "./CustomerSearch"
-import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearch"
+import CustomerSearch from "./CustomerSearch";
+import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearch";
+import * as Sentry from "@sentry/nextjs";
+import CustomerTable from "./CustomerTable";
 
 export const metadata = {
-    title : "Customer Search",
-}
+  title: "Customer Search",
+};
 
-export default async function Customers({searchParams,} : {searchParams : Promise<{[key : string] : string | undefined}>} ) {
-  const { searchText } = await searchParams
-  if(!searchText) return <CustomerSearch />
+export default async function Customers({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+  if (!searchText) return <CustomerSearch />;
 
+  const span = Sentry.startInactiveSpan({
+    name : 'getCustomerSearchResults-2'
+  })
   // query database
-  const results = await getCustomerSearchResults(searchText)
+  const results = await getCustomerSearchResults(searchText);
+  span.end()
 
   return (
     <>
       <CustomerSearch />
-      <p>{JSON.stringify(results)}</p>
+      { results.length ? <CustomerTable data={results}/> : (
+        <p className="mt-4"> No results found </p>
+      )}
     </>
-  )
+  );
 }
